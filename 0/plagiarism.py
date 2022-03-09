6935a7a29f48bebaf14aa10c9f5076a51e233803
Nora Beringer <56232123+noraberinger@users.noreply.github.com>
2022-03-09 19:38:14 +0100

    Add files via upload

---
+++ 0/plagiarism.py
@@ -0,0 +1,15 @@
+'''
+    Name of group members:
+        Laurin van den Bergh
+        Nora Beringer
+        Yufeng Xiao
+'''
+def printAnwsers():
+    print('------------------------------------------------------------------------------------')
+    print('The following behaviours are considered as plagiarism: ')
+    print('    1. Copy the solution of another exercise group')
+    print('    2. Copy from the sample solution of previous years')
+    print('    3. Altering existing code by renaming variables or swapping lines where possible')
+    print('------------------------------------------------------------------------------------')
+
+printAnwsers()
