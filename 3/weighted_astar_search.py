9168ca5f177cf1bd48dbe6410651a41bd2c66ba9
Nora Beringer <56232123+noraberinger@users.noreply.github.com>
2022-03-16 13:49:40 +0100

    Add files via upload

--- 3/weighted_astar_search.py
+++ 3/weighted_astar_search.py
@@ -25,6 +25,50 @@ class WeightedAStarSearch(Search):
 
     # enqueue initial state
     frontier = PriorityQueue()
+    frontier.put(SearchNode(p.initial_state, None, 0))
+    self.generated += 1
+    reached = {p.initial_state}
+
+    while not frontier.empty():
+      node = frontier.get()
+      self.expanded += 1
+
+      for action in p.actions(node.state):
+        succ, cost = p.result(node.state, action)
+        new_g = node.g + cost
+        succ_node = SearchNode(succ, node, new_g)
+
+        # early goal test
+        if p.is_goal(succ):
+          return self.extract_path(succ_node), new_g
+
+        # mark reached to avoid cycles
+        if succ not in reached:
+          reached.add(succ)
+
+          # enqueue successor
+          frontier.put(succ_node)
+          self.generated += 1
+
+        if self.generated == self.max_generations:
+          print("Aborting search after generating " +
+            f"{self.max_generations} states without finding a solution.")
+          return None, None
+
+    # no solution found
+    print("Explored entire search problem, no solution exists.")
+    return None, None
+
+"""
+    #first own try on the solution
+
+    # early goal test for initial state
+    p = self.search_problem
+    if p.is_goal(p.initial_state):
+      return [p.initial_state], 0
+
+    # enqueue initial state
+    frontier = PriorityQueue()
     frontier.put(SearchNode(p.initial_state, None, 0)) #g(s)
     self.generated += 1
     open = {} #set of candidates for expansion
@@ -61,7 +105,8 @@ class WeightedAStarSearch(Search):
 
     # no solution found
     print("Explored entire search problem, no solution exists.")
-    return None, None    
+    return None, None  
+"""      
 
 if __name__ == "__main__":
   problem = pancake_problem.generate_random_problem(5)
